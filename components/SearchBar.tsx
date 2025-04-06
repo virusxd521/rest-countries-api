386a0abbeb8bc9cb21ff5b9dd7c2867346aa431f
Daniel Benisti <daniel.benisti@alteryx.com>
2025-04-06 21:25:47 +0200

    Update: add filter-by-region, add search-bar, add react-query

---
+++ components/SearchBar.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import { Input } from "@/components/ui/input";
+
+const SearchBar = () => {
+  return (
+    <Input type="text" placeholder="Search for a country..." />
+  )
+};
+
+export default SearchBar;
