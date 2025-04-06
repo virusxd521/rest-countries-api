611f93272ede7c8bb18bf348723d2dbff899756e
Daniel Benisti <daniel.benisti@alteryx.com>
2025-04-06 22:13:17 +0200

    Update: add cards, add inital connected to API, add Searchbar event listener

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,17 @@
 import Header from '@/components/Header';
 import SearchBar from "@/components/SearchBar";
-import FilterByRegoin from "@/components/FilterByRegion";
+import FilterByRegion from "@/components/FilterByRegion";
+import Cards from "@/components/Cards";
 
 export default function Home() {
   return (
-    // TODO: fix grid styling
     <div className="grid">
       <Header />
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         {/* TODO: finish both the searchbar and the filter*/}
         <SearchBar />
-        <FilterByRegoin />
+        <FilterByRegion />
+        <Cards />
       </main>
     </div>
   );
